c1e119c6680eb713b52663f313ee6fb622935cf7
DefoltFES <mironoff.emil@yandex.ru>
2021-03-10 15:32:11 +0300

    Initial commit

---
+++ ClassLibrary/Library.fs
@@ -0,0 +1,11 @@
+﻿namespace ClassLibrary
+
+module Sort =
+   let rec qsort = function
+   | [] -> []
+   | (x:int) :: xs ->
+       let smaller = List.filter ((>=) x) xs
+       let larger  = List.filter ((<) x) xs
+       qsort smaller @ [x] @ qsort larger
+    
+        
---
+++ QuickSortC/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using ClassLibrary;
+using Microsoft.FSharp.Collections;
+
+namespace QuickSortC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> array = new List<int>() { -22, 2, 34, -2, 0, 9, -5, 14, -55, 74, 13};
+            Console.Write("Not sorrted array \n");
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+            FSharpList<int> arrayFunc = ListModule.OfSeq(array);
+            array = new List<int>(ListModule.ToSeq(Sort.qsort(arrayFunc)));
+            Console.WriteLine();
+            Console.Write("Sorted array \n");
+            foreach (var item in array)
+            {
+                Console.Write(item+" ");
+            }
+            
+        }
+
+      
+    }
+}
